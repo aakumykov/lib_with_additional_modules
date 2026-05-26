539f5f014dca08ea7528f13d9668850e36210f23
Andrey Kumykov <aakumykov@yandex.ru>
2026-05-26 21:13:18 +0300

    lib_0: v0.0.3 (Lib0 использует Lib1 и Lib2)

--- app/src/main/java/com/github/aakumykov/lib_with_additional_modules/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/lib_with_additional_modules/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.aakumykov.lib_0.Lib0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Lib0().work()
     }
 }
--- lib_0/src/main/java/com/github/aakumykov/lib_0/Lib0.kt
+++ lib_0/src/main/java/com/github/aakumykov/lib_0/Lib0.kt
@@ -1,11 +1,15 @@
 package com.github.aakumykov.lib_0
 
 import android.util.Log
+import com.github.aakumykov.lib_1.Lib1
+import com.github.aakumykov.lib_2.Lib2
 import kotlin.jvm.java
 
 class Lib0 {
     fun work() {
         Log.d(TAG, "work(v0.0.2)")
+        Lib1().work()
+        Lib2().work()
     }
     companion object {
         val TAG: String = Lib0::class.java.simpleName
